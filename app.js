95777ad736bfe74566f5249071b853864d62631c
VALERUKI <150066908+valeruki@users.noreply.github.com>
2023-11-10 17:38:20 -0500

    Inicio

---
+++ app.js
@@ -0,0 +1,112 @@
+// Elements ****************************************************
+
+const main = document.querySelector('.main')
+const cardTemplate = document.querySelector('#cardTemplate').content
+let fragment = document.createDocumentFragment()
+const searchInput = document.querySelector('#searchInput')
+const autoComplete = document.querySelector('.autocomplete')
+const filtersBtn = document.querySelector('#filtersBtn')
+const filter = document.querySelector('.filter')
+const closeFilterBtn = document.querySelector('#closeFilterBtn')
+
+const filterGroupDama = document.querySelector('#groupDama')
+const filterGroupCaballero = document.querySelector('#groupCaballero')
+const filterGroupInfantil = document.querySelector('#groupInfantil')
+
+// load filter options ********************************************
+
+categories.map((i) => { 
+    if (i.group == "Dama"){
+        filterGroupDama.innerHTML += `<p class="filter__item" data-id=${i.id}>${i.name}</p>`        
+    }
+    if (i.group == "Caballero"){
+        filterGroupCaballero.innerHTML += `<p class="filter__item" data-id=${i.id}>${i.name}</p>`        
+    }
+    if (i.group == "Infantil"){
+        filterGroupInfantil.innerHTML += `<p class="filter__item" data-id=${i.id}>${i.name}</p>`        
+    }   
+})
+
+// Functions ****************************************************
+
+const enableShower = () => {
+    const cardShower = document.querySelectorAll('.card__shower')
+    for (let i = 0; i < cardShower.length; i++){
+        cardShower[i].addEventListener('click', () => {
+            cardShower[i].previousElementSibling.classList.toggle('hidden')
+            cardShower[i].childNodes[1].classList.toggle('shower-indicator')
+        })
+    }
+}
+
+const loadCatalog = (data) => {
+    main.innerHTML = ''
+    for (let i = 0; i < data.length; i++){        
+        let cloneTemplate = cardTemplate.cloneNode(true)
+        
+        for (let j = 0; j < data[i].image.length; j++){
+            cloneTemplate.querySelector('.card__img__container').innerHTML += `<img loading="lazy" class="card__img" src="./images/${data[i].image[j]}" alt="VALERUKI">`
+        }
+        
+        cloneTemplate.querySelector('.card').setAttribute('href', `https://daedsoft.github.io/VALERUKI_CATALOG/item.html?id=${data[i].id}`)
+        cloneTemplate.querySelector('.card__title').textContent =  data[i].title
+        cloneTemplate.querySelector('.card__main-info__cat').textContent = categories.filter(c => c.id == data[i].category)[0].name 
+        cloneTemplate.querySelector('.card__main-info__price').textContent = data[i].price        
+        fragment.appendChild(cloneTemplate)
+    }
+    
+    if (data.length == 0){
+        main.innerHTML = `<h3 class='info'>Sin resultados</h3>`
+    }else{
+        main.appendChild(fragment)
+    }    
+    //enableShower()
+}
+
+const searchInCatalog = () => {
+    let getSearch = searchInput.value
+    let expression = new RegExp(`${getSearch}.*`, 'i')
+    const catalogFiltered = catalog.filter(item => expression.test(item.title))     
+    loadCatalog(catalogFiltered)
+}
+
+const filterInCatalog = (selected) => {
+    if (selected == '0'){
+        loadCatalog(catalog)
+    }else{        
+        const catalogFiltered = catalog.filter(item => item.category == selected)     
+        loadCatalog(catalogFiltered)
+    }    
+}
+
+// Listeners *************************************************************
+
+window.addEventListener('DOMContentLoaded', () => {
+    catalog.reverse()
+    loadCatalog(catalog)
+})
+
+searchInput.addEventListener('input', () => {
+    searchInCatalog(catalog)
+})
+
+filtersBtn.addEventListener('click', () => {
+    filter.classList.toggle('hidden-outside')
+})
+
+closeFilterBtn.addEventListener('click', () => {
+    filter.classList.toggle('hidden-outside')
+})
+
+const filterItems = document.querySelectorAll('.filter__item')
+for (let i = 0; i < filterItems.length; i++){
+    filterItems[i].addEventListener('click', () => {
+        filterInCatalog(filterItems[i].getAttribute("data-id"))
+        filter.classList.add('hidden-outside')
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        })
+    })
+}
---
+++ data.js
@@ -0,0 +1,68 @@
+intro = `<p class="c-gray">Piezas tejidas a mano por artesanos de la ciudad de Sincelejo - Sucre.</p><br>`
+
+cuidados = `    
+    <ul class='list'>
+        <li>Evita el contacto con productos químicos o de aseo en general.</li>
+        <li>Las cremas, el perfume y el agua pueden alterar su color original.</li>
+        <li>Evita golpear tu joya.</li>
+        <li>No la uses para bañarte en piscinas o en el mar. </li>
+        <li>Guárdala por separado para evitar desgaste o enredos por roce con otros accesorios.</li>
+    </ul>    
+    
+`
+
+categories = [
+    {"id": 1, "group": "Dama", "name": "Pulsera"},
+    {"id": 2, "group": "Dama", "name": "Collar"},
+    {"id": 3, "group": "Dama", "name": "Aretes"},
+    {"id": 4, "group": "Dama", "name": "Tobillera"},
+    {"id": 5, "group": "Dama", "name": "Anillo"},
+    {"id": 6, "group": "Dama", "name": "Dije"},    
+    {"id": 7, "group": "Caballero", "name": "Pulsera"},
+    {"id": 6, "group": "Infantil", "name": "Pulsera"}
+]
+
+catalog = [   
+
+    {"id": "001", 
+    "title": "JOYAS CON PIEZAS DE ORO 18 KILATES", 
+    "category": 1, 
+    "price" : "$51.000", 
+    "image": ["001-01.jpg"],
+    "description": "Sistema de cierre con nudo corredizo para graduar a tu muñeca. <br> <b>Materiales:</b><br>Mostacilla japonesa.<br>Hilo encerado. <br>1 balin de oro 18 quilates N°3mm."},
+
+    {"id": "002", 
+    "title": "ANILLO", 
+    "category": 5, 
+    "price" : "$35.000", 
+    "image": ["002-01.jpg"],
+    "description": "Se realiza a la medida del cliente. <br> <b>Materiales:</b><br>Mostacilla japonesa.<br>Hilo encerado."}, 
+
+    {"id": "003", 
+    "title": "COLLAR CHOKER", 
+    "category": 2, 
+    "price" : "$25.000", 
+    "image": ["003-01.jpg"],
+    "description": "<b>Materiales:</b><br>Mostacilla japonesa.<br>Sistema de cierre con herrajes de acero dorado.<br>Medida aproximada de choker entre 38 a 40 cm con extensión de cadena."}, 
+
+    {"id": "004", 
+    "title": "JOYAS PARA COMPARTIR", 
+    "category": 4, 
+    "price" : "$90.000", 
+    "image": ["004-01.jpg", "004-01.jpg", "004-01.jpg"],
+    "description": "Sistema de cierre con nudo corredizo para graduar a tu muñeca. <br><b>Materiales:</b><br>Mostacilla japonesa.<br>Hilo encerado."}, 
+
+    
+]
+
+/*
+
+    {"id": "", 
+    "title": "", 
+    "category": , 
+    "price" : "$", 
+    "image": [".jpg"],
+    "description": ""}, 
+
+*/
+
